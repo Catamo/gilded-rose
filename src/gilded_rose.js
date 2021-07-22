85ad3d4dbbd034e196eaf93bc95f1c9b4d68990e
catamo <victor.catamo.rojas@gmail.com>
2021-07-22 10:17:45 +0200

    Move item specific logic into handler functions

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -27,42 +27,64 @@ class Shop {
     this.items = items;
   }
 
+  handleAgedBrie = function(item) {
+    item.decreaseSellInBy(1);
+
+    item.increaseQualityBy(1);
+
+    if (item.sellIn < 0) {
+      item.increaseQualityBy(1);
+    }
+
+    return item
+  }
+  
+  handleBackstagePass = function(item) {
+    item.decreaseSellInBy(1);
+
+    item.increaseQualityBy(1);
+
+    if (item.sellIn < 11) {
+      item.increaseQualityBy(1);
+    } 
+    if (item.sellIn < 6) {
+      item.increaseQualityBy(1);
+    }
+
+    if (item.sellIn < 0) {
+      item.decreaseQualityBy(item.quality);
+    }
+
+    return item
+  }
+
+  handleSulfuras = function(item) {
+    return item
+  }
+
+  handleDefault = function(item) {
+    item.decreaseSellInBy(1);
+
+    item.decreaseQualityBy(1);
+
+    if (item.sellIn < 0) {
+      item.decreaseQualityBy(1);
+    }
+
+    return item
+  }
+
   updateQuality() {
     for (let i = 0; i < this.items.length; i++) {
       if (this.items[i].name == 'Aged Brie') {
-        this.items[i].decreaseSellInBy(1);
-
-        this.items[i].increaseQualityBy(1);
-
-        if (this.items[i].sellIn < 0) {
-          this.items[i].increaseQualityBy(1);
-        }
+        this.items[i] = this.handleAgedBrie(this.items[i])
       }
       else if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
-        this.items[i].decreaseSellInBy(1);
-
-        this.items[i].increaseQualityBy(1);
-
-        if (this.items[i].sellIn < 11) {
-          this.items[i].increaseQualityBy(1);
-        } 
-        if (this.items[i].sellIn < 6) {
-          this.items[i].increaseQualityBy(1);
-        }
-
-        if (this.items[i].sellIn < 0) {
-          this.items[i].decreaseQualityBy(this.items[i].quality);
-        }
+        this.items[i] = this.handleBackstagePass(this.items[i])
       }
       else {
         if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-          this.items[i].decreaseSellInBy(1);
-          
-          this.items[i].decreaseQualityBy(1);
-
-          if (this.items[i].sellIn < 0) {
-            this.items[i].decreaseQualityBy(1);
-          }
+          this.items[i] = this.handleDefault(this.items[i])
         }
       }
     }
